2abbfceb639ccb7b9e0a7862dbd66e04b272e00b
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2023-11-17 12:09:15 +0400

    chore(release): 0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### 0.0.1 (2023-11-17)
+
+
+### Features
+
+* add packages ([51e1ca9](https://github.com/nikitaKovalev/toh-pt6/commit/51e1ca984bc0f179895279dd2fa1cbe2d035fa30))
+* injectable functions ([0abc65e](https://github.com/nikitaKovalev/toh-pt6/commit/0abc65e24858d844e1bcab9a0f1a0a7dffa6c99d))
+* schematics ([5c9220b](https://github.com/nikitaKovalev/toh-pt6/commit/5c9220b98ad1928c7012efff9c9613aefa048ee9))
---
+++ scripts/shared/update-package-json-structure.ts
@@ -0,0 +1,61 @@
+import {tuiIsObject} from '../../projects/cdk/utils/miscellaneous/is-object';
+import {tuiIsString} from '../../projects/cdk/utils/miscellaneous/is-string';
+import {bumpTuiDeps} from './bump-tui-deps';
+import {bumpTuiVersionInPackageJson} from './bump-tui-version-in-package-json';
+import {isTuiPackageName} from './is-tui-package-name';
+
+interface UpdatePackageJsonOptions {
+    ignores: string[];
+    isPackageLockFile: boolean;
+    newVersion: string;
+    packageJson: Record<string, Record<string, any> | string>;
+    prevVersion: string;
+}
+
+export function updatePackageJsonStructure({
+    packageJson,
+    isPackageLockFile,
+    ignores,
+    prevVersion,
+    newVersion,
+}: UpdatePackageJsonOptions): void {
+    const {name, dependencies, peerDependencies, devDependencies, packages} = packageJson;
+
+    if (tuiIsString(name) && isTuiPackageName(name, ignores)) {
+        bumpTuiVersionInPackageJson(packageJson, newVersion);
+    }
+
+    if (tuiIsObject(dependencies)) {
+        bumpTuiDeps({deps: dependencies, prevVersion, newVersion, ignores});
+    }
+
+    if (tuiIsObject(peerDependencies)) {
+        bumpTuiDeps({
+            deps: peerDependencies,
+            prevVersion,
+            newVersion,
+            isPeerDependency: true,
+            ignores,
+        });
+    }
+
+    if (tuiIsObject(devDependencies)) {
+        bumpTuiDeps({deps: devDependencies, prevVersion, newVersion, ignores});
+    }
+
+    if (isPackageLockFile && tuiIsObject(packages)) {
+        for (const packageLockJson of Object.values(packages)) {
+            if (!isTuiPackageName(packageLockJson?.name, ignores)) {
+                continue;
+            }
+
+            updatePackageJsonStructure({
+                packageJson: packageLockJson,
+                prevVersion,
+                newVersion,
+                isPackageLockFile: true,
+                ignores,
+            });
+        }
+    }
+}
